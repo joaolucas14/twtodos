3dc203222d8c6e9e29687205c5d0e280af6ba610
joaolucas14 <jlparceiro10@gmail.com>
2024-02-26 15:50:34 -0300

    add git ignore

--- todos/models.py
+++ todos/models.py
@@ -1,12 +1,16 @@
 from django.db import models
 from datetime import date
 
+
 class Todo(models.Model):
     title = models.CharField(
         verbose_name="Título", max_length=100, null=False, blank=False
     )
     created_at = models.DateField(auto_now_add=True, null=False, blank=False)
-    deadline = models.DateField(verbose_name="Data de entrega", null=False, blank=False)
+    deadline = models.DateField(
+        verbose_name="Data de entrega",
+        null=False,
+        blank=False)
     finished_at = models.DateField(null=True, blank=True)
 
     class Meta:
