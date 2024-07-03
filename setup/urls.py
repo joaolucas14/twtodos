7ab767e37c2e7c5bcab3fb896f295bddb746d89a
joaolucas14 <jlparceiro10@gmail.com>
2024-07-03 18:14:19 -0300

    Separando informações do banco

--- setup/urls.py
+++ setup/urls.py
@@ -1,9 +1,8 @@
-
 from django.contrib import admin
 from django.urls import path
 from todos.views import home
 
 urlpatterns = [
-    path('', home),
-    path('admin/', admin.site.urls),
+    path("", home),
+    path("admin/", admin.site.urls),
 ]
